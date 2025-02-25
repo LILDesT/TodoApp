9f1b0938888a2ab69bc8204905920c8c6cf61ccb
Shamil Mazitov <shamiltron@yandex.com>
2025-02-25 17:31:26 +0300

    `Added FastAPI request handling and Jinja2 templates`

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from database import engine
+
 import models
 from routers import auth, todos, admin, users
-
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+templates = Jinja2Templates(directory="templates")
 
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(users.router)
-
